90e4fdd24115733f402cd4e6c1d2e8ccbb73d76d
mario <mariostarkill@gmail.com>
2024-07-25 08:52:00 -0500

    1 y 2 Ejercicios

---
+++ src/Ejercicio1.java
@@ -0,0 +1,5 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int x = (int) 5.89 + (int) 3.45;
+        System.out.println(x);
+}}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args){
+        char c = 'A';
+        int asciiValue = (int) c;
+
+        System.out.println("El valor ASCII de '" + c + "' es: " + asciiValue);
+    }}
